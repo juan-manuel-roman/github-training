a2e4f87312c27c71b2cfa30ad1583bbccdef15b0
Juanro <manuel.roman@wizeline.com>
2020-08-11 13:06:17 -0500

    Moved the file to the right directory
    - File was not in the correct location

--- basic_class.js
+++
@@ -1,9 +0,0 @@
-class MyClass {
-  constructor(){
-    this.name = "This is my class";
-  }
-
-  name() {
-    console.log(this.name);
-  }
-}
--- test/basic_math.js
+++ test/basic_math.js
@@ -1,7 +1,9 @@
-const sum = elements => elements.reduce((a, b) => a + b);
-const diff = (a, b) => a - b;
-const mult = (elements) => elements.reduce((a, b) => a * b);
-const divide = (a, b) => a / b;
+class MyClass {
+  constructor(){
+    this.name = "This is my class";
+  }
 
-module.exports = { sum, diff, mult, divide };
-
+  name() {
+    console.log(this.name);
+  }
+}
